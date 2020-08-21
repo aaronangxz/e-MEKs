7fdb6a4798242706f02d9ef8c904069b0211f500
aaronangxz <angxze@gmail.com>
2020-08-22 00:39:02 +0800

    21 Aug Updates

--- src/pages/Resources.js
+++ src/pages/Resources.js
@@ -2,7 +2,7 @@ import React from 'react';
 import '../App.css';
 import styled from 'styled-components';
 import SafeArea from 'react-safe-area-component'
-import { SearchInput, Card} from 'react-onsenui';
+import { SearchInput} from 'react-onsenui';
 import 'onsenui/css/onsenui.css';
 import 'onsenui/css/onsen-css-components.css';
 import notes_ico from './Resources/notes_ico.png'
@@ -11,8 +11,8 @@ import journals_ico from './Resources/journals_ico.png'
 import assaysheets_ico from './Resources/assaysheets_ico.png'
 import datasheets_ico from './Resources/datasheets_ico.png'
 import dws_ico from './Resources/dws_ico.png'
-
-
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 const Wrapper = styled.div`
   display: flex;
@@ -43,30 +43,60 @@ const img1 = styled.div`
 `; */}
 
 
+const Resources = (props) => {
+  return (
+      
+      
+      <div>    
+   
+   
+
+
+      </div>
+    )
+  };
+
+
+
+
 const ExampleStyledComponents = () => (
   <SafeArea top bottom>
   <Wrapper>
     <Title>Resources</Title>
-    <Card>
-    <img src = {notes_ico} alt="notes_ico"/>
-    </Card>
-    <Card>
-    <img src = {signals_ico} alt="signals_ico"/>
-    </Card>
-    <Card>
-    <img src = {journals_ico} alt="journals_ico"/>
-    </Card>
-    <Card>
-    <img src = {assaysheets_ico} alt="assaysheets_ico"/>
-    </Card>
-    <Card>
-    <img src = {datasheets_ico} alt="datasheets_ico"/>
-    </Card>
-    <Card>
-    <img src = {dws_ico} alt="dws_ico"/>
-    </Card>
+    <Card className="text-center" style={{ display:'inline', justifyContent:'top' , width: 'devicewidth'}}>
+  <Card.Body style={{height: '80px',padding: '0px'}} >
+    <Card.Img variant = "top" style={{width: '6vh'}} src={notes_ico}/>
+    <Card.Title>Technical Notes</Card.Title>
+  </Card.Body>
+</Card>
+<Card className="text-center" style={{ display:'inline', justifyContent:'top' , width: 'devicewidth'}}>
+  <Card.Body style={{height: '80px',padding: '0px'}} >
+    <Card.Img variant = "top" style={{width: '6vh'}} src={datasheets_ico}/>
+    <Card.Title>Data Sheets</Card.Title>
+  </Card.Body>
+</Card>
+<Card className="text-center" style={{ display:'inline', justifyContent:'top' , width: 'devicewidth'}}>
+  <Card.Body style={{height: '80px',padding: '0px'}} >
+    <Card.Img variant = "top" style={{width: '6vh'}} src={assaysheets_ico}/>
+    <Card.Title>Assay Sheets</Card.Title>
+  </Card.Body>
+</Card>
+<Card className="text-center" style={{ display:'inline', justifyContent:'top' , width: 'devicewidth'}}>
+  <Card.Body style={{height: '80px',padding: '0px'}} >
+    <Card.Img variant = "top" style={{width: '6vh'}} src={journals_ico}/>
+    <Card.Title>Journals</Card.Title>
+  </Card.Body>
+</Card>
+<Card className="text-center" style={{ display:'inline', justifyContent:'top' , width: 'devicewidth'}}>
+  <Card.Body style={{height: '80px',padding: '0px'}} >
+    <Card.Img variant = "top" style={{width: '6vh'}} src={signals_ico}/>
+    <Card.Title>Signals</Card.Title>
+  </Card.Body>
+</Card>
+
 
   </Wrapper>
   </SafeArea>
 );
+//export default ExampleStyledComponents;
 export default ExampleStyledComponents;
